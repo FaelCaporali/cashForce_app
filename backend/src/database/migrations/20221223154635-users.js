645e329a4b6b3e3ba3c022b95655444509a51d4d
Fael Caporali <rafaelhon@gmail.com>
2022-12-23 16:22:20 -0300

    [feat - back] users migration

--- backend/src/database/migrations/20221223154635-users.js
+++ backend/src/database/migrations/20221223154635-users.js
@@ -3,20 +3,62 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      mobile: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      department: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      verificationCode: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      emailChecked: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      cashForceAdm: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      }
+    }, {
+      initialAutoIncrement: 122,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
   }
 };
