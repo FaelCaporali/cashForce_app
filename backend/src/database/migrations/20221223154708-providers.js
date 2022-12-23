f3f51b88e4d0d7544a5d9c9f2a8a006fdbb14b95
Fael Caporali <rafaelhon@gmail.com>
2022-12-23 20:25:59 -0300

    [feat - back] add providers migration

--- backend/src/database/migrations/20221223154708-providers.js
+++ backend/src/database/migrations/20221223154708-providers.js
@@ -3,20 +3,128 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('providers', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      tradingName: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      cashforceTax: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      responsibleName: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      responsibleEmail: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      responsiblePosition: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      responsiblePhone: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      responsibleMobile: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      website: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      postalCode: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      address: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      number: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      complement: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      city: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      state: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      bank: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      bankAgency: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      account: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      documents: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      situation: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      situationDate: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'cnpj', key: 'cnpj' },
+        key: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },      
+    }, {
+      initialAutoIncrement: 235,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('providers');
   }
 };
