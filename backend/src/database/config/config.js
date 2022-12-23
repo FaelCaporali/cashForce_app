ed0dbbfe9a1a50d45adf26fcbb1f9174b64f50cf
Fael Caporali <rafaelhon@gmail.com>
2022-12-23 17:06:13 -0300

    [chore -back] add environment variables

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv')
+  .config({
+    path: require('path')
+      .resolve(__dirname, '../../../.env'),
+  });
+
 const config = {
-  username: 'root',
-  password: '123456',
-  database: 'cashForce_dev',
-  host: 'localhost',
-  port: 3002,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || undefined,
+  database: process.env.DB_NAME || 'app-db',
+  host: process.env.HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
