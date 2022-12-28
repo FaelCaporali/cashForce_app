a24505ae0b5202758e2d91f026211bb26e178886
Fael Caporali <rafaelhon@gmail.com>
2022-12-28 20:11:02 -0300

    [feat - back] add Order Migration

--- backend/src/database/migrations/20221223154847-ordersportions.js
+++ backend/src/database/migrations/20221223154847-ordersportions.js
@@ -3,20 +3,56 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('orderportions', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nDup: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      dVenc: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      vDup: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      availableToMarket: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'orders', key: 'id' },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+        key: true,
+      }
+    }, {
+      initialAutoIncrement: 612,
+      engine: 'InnoDB',
+      charset: 'latin1',
+      collate: 'latin1_swedish_ci',
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('orderportions');
   }
 };
