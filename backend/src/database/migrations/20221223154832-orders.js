b30f8b44757ead448e276020dc9f744e95bc09c8
Fael Caporali <rafaelhon@gmail.com>
2022-12-28 18:05:28 -0300

    [feat - back] add orders migration + seed

--- backend/src/database/migrations/20221223154832-orders.js
+++ backend/src/database/migrations/20221223154832-orders.js
@@ -3,20 +3,129 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      orderNfId: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      orderNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      orderPath: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+        unique: true,
+      },
+      orderFileName: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+        unique: true,
+      },
+      orderOriginalName: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+        unique: true,
+      },
+      emissionDate: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      pdfFile: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      emitedTo: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      nNf: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      CTE: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      value: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'cnpjs', key: 'id' },
+        key: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'users', key: 'id' },
+        key: true,
+      },
+      buyerId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'buyers', key: 'id' },
+        key: true,
+      },
+      providerId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: { model: 'providers', key: 'id' },
+        key: true,
+      },
+      orderStatusBuyer: {
+        type: Sequelize.STRING(255),
+        defaultValue: '0',
+        validate: {
+          is: /^[0-8]$/i,
+        },
+      },
+      orderStatusProvider: {
+        type: Sequelize.STRING(255),
+        defaultValue: '0',
+        validate: {
+          is: /^[0-8]$/i,
+        },
+      },
+      deliveryReceipt: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      cargoPackingList: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      deliveryCtrc: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+    }, {
+      initialAutoIncrement: 540,
+      engine: 'InnoDB',
+      charset: 'latin1',
+      collate: 'latin1_swedish_ci',
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('orders');
   }
 };
