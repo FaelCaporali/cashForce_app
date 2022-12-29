48e18ca98e671ee044167f1d4c4f9c1793cd23aa
Fael Caporali <rafaelhon@gmail.com>
2022-12-29 00:33:04 -0300

    [feat - back] add orders routes

---
+++ backend/src/routes/Orders.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import OrdersCtl from '../controllers/Orders.controller';
+
+export default class OrderRouter {
+  declare readonly router: Router;
+  declare private readonly ctl: typeof OrdersCtl;
+
+  constructor() {
+    this.router = Router();
+    this.ctl = OrdersCtl;
+    this.initiatePublicRoutes();
+  }
+
+  private initiatePublicRoutes(): void {
+    this.router.get('/', async (req, res, next) => await new this.ctl(req, res, next).listAll());
+  }
+}
