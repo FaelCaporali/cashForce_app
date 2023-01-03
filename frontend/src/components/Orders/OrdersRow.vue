4c1ad061cbda972cc70c6595649302fda4c8f938
Fael Caporali <rafaelhon@gmail.com>
2023-01-03 18:28:20 -0300

    [refact - front] format date and value

--- frontend/src/components/Orders/OrdersRow.vue
+++ frontend/src/components/Orders/OrdersRow.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { IOrder } from "../../interfaces";
 import ProviderLink from "../shared/ProviderLink.vue";
+import { formatDate, formatValue } from "../../helpers/formatters";
 const props = defineProps<{ order: IOrder }>();
 
 const statusMapper = [
@@ -21,17 +22,76 @@ const statusMapper = [
     <td>{{ props.order.nNf }}</td>
     <td>{{ props.order.buyer.name }}</td>
     <td>{{ props.order.provider.name }}</td>
-    <td>{{ props.order.emissionDate }}</td>
-    <td>{{ props.order.value }}</td>
-    <td>{{ statusMapper[props.order.orderStatusBuyer] }}</td>
-    <td><ProviderLink v-bind:providerId="props.order.provider.id" /></td>
+    <td>{{ formatDate(props.order.emissionDate) }}</td>
+    <td class="value">{{ formatValue(props.order.value) }}</td>
+    <td class="status">{{ statusMapper[props.order.orderStatusBuyer] }}</td>
+    <td class="link-btn">
+      <ProviderLink v-bind:providerId="props.order.provider.id" />
+    </td>
   </tr>
 </template>
 
 <style scoped>
 tr {
   display: flex;
-  align-items: baseline;
-  justify-content: center;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  border: 1px solid #dfe2eb;
+  border-radius: 6px;
+}
+td {
+  text-align: center;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0 3%;
+
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+
+  color: #4d5566;
+}
+
+.value {
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+
+  color: #00ad8c;
+}
+
+.status {
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 16px;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: #00ad8c;
+}
+
+.link-btn {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  border: 1px solid #cad3ff;
+  border-radius: 24px;
+  margin: 1%;
 }
 </style>
